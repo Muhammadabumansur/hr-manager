7774d1e7431d3f28d444727c0abecbaaa1867c8b
magomed.kandaev <muhammad-114@mail.ru>
2018-01-28 21:41:33 +0300

    Initialize project, make components Make components for applicants table, filter panel and pagination. Make pagination. Didn't use router because haven't experience. Didn't make filtering.

---
+++ src/components/ApplicantsList.vue
@@ -0,0 +1,29 @@
+<template>
+      <table class="table table-striped">
+        <thead class="thead-dark">
+          <tr>
+            <th v-for="(col, index) in cols" :key="index">
+              {{ col }}
+            </th>
+          </tr>
+        </thead>
+        <tr v-for="(applicant, index) in applicants" :key="index">
+          <td v-for="(value, index) in applicants[index]" :key="index">{{ value }}</td>
+        </tr>
+      </table>
+</template>
+
+<script>
+export default {
+  props: ['applicants', 'cols'],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/components/ApplicantsPagination.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <select class="form-control" name="" id="" @change="onChange($event)">
+      <option value="10" selected>10</option>
+      <option value="20">20</option>
+      <option value="30">30</option>
+    </select>
+    <ul class="pagination">
+      <li v-for="(item, index) in pagesQuantity" :key="index">
+        <a href="" @click.prevent="onClick(index)">
+          {{ index + 1 }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['showNumber', 'pagesQuantity'],
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    onChange(event) {
+      let num = Number(event.target.value);
+      this.$emit('changeNumber', num)
+    },
+    onClick(index) {
+      this.$emit('changePage', index)
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
